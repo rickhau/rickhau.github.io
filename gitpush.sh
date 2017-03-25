69db0b55ec2f711347f9659883b865aff80e2995
Rick Lin <rickhau@gmail.com>
2017-03-25 20:26:11 +0800

    Update new content to pelican blogger...

--- gitpush.sh
+++ gitpush.sh
@@ -1,3 +1,11 @@
+# Use "source" branch to create new post under "content" folder
+# Commit and push back to remote origin/source branch
+# Travis CI will run the Makefile 
+# Makefile
+# - Run pelicanconf first (So, you have to checkout git repo in .travis.yml before here)
+# - Then, use ghp-import to push the compiled source back to master branch
+
+# Create source post in content folder and run the following commands
 git pull origin source
 git add .
 git commit -am "Update new content to pelican blogger..."
