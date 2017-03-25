3025d3c36ec10140afc1be98dc0c86db44e0c179
Rick Lin <rickhau@gmail.com>
2017-03-25 22:16:49 +0800

    Update new content to pelican blogger...

--- content/2015/05/08/2015-05-08-install-libnfs-python-library.md
+++ content/2015/05/08/2015-05-08-install-libnfs-python-library.md
@@ -179,3 +179,5 @@ Reference
 1) [Discussion with Author](https://github.com/sahlberg/libnfs-python/issues/1)    
 2) [Step-by-Step Video](https://asciinema.org/a/19474)   
 3) [Python Build](https://docs.python.org/2/extending/building.html)
+
+![Test Image](https://github.com/rickhau/rickhau.github.io/tree/source/content/images/test.jpg)
--- gitpush.sh
+++ gitpush.sh
@@ -1,3 +1,13 @@
+# HOW-TO
+# Reference: http://mathamy.com/migrating-to-github-pages-using-pelican.html
+# Use virtual env to install pelican, markdown
+# git clone https://github.com/getpelican/pelican-plugins.git
+# git clone https://github.com/fle/pelican-simplegrey.git
+# Above two git folders will not be pushed due to .gitignore
+# Create Article: python make_entry.py "Your Title"
+# Edit your "Your Title".md
+# Run ./gitpush.sh
+
 # Use "source" branch to create new post under "content" folder
 # Commit and push back to remote origin/source branch
 # Travis CI will run the Makefile 
