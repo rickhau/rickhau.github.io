25f15fab645c89e70aa27eff15fb772d39aa7451
rickhau <rickhau@gmail.com>
2017-04-05 23:48:33 +0800

    Change to disqus_static plugin

--- pelicanconf.py
+++ pelicanconf.py
@@ -49,7 +49,7 @@ PLUGIN_PATHS = ['pelican-plugins']
 
 # For pelican-bootstrap3 theme settings
 # FAVICON= "images/favicon.ico"
-PLUGINS = ['pelican_comment_system']
+PLUGINS = ['disqus_static']
 # PLUGINS = ['assets', 'i18n-subsites']  # i18n-subtitles: this is for pelican-bootstrap3 theme
 #JINJA_ENVIRONMENT = {
 #    'extensions': ['jinja2.ext.i18n'], #this is for pelican-bootstrap3 theme
